e1797f42c00820a7965318323604cd61d78bc90d
wjy <stephenwang11@qq.com>
2019-12-16 20:30:47 +0800

    新建mapreduce mapper 用来切分单词，并输出给shuflle

---
+++ src/main/java/com/alibaba/hadoopdemo/hadoop/mapreduce/wordcount/WordCountMapper.java
@@ -0,0 +1,10 @@
+package com.alibaba.hadoopdemo.hadoop.mapreduce.word_count;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ * @Author: wjy
+ * @Date: 2019/12/16 19:58
+ */
+public class WordCountMapper extends Mapper {
+}
