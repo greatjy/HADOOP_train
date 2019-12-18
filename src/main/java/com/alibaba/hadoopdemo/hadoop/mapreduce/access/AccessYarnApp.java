14422632596f7671c57e738e0ee86b70db9d067f
wjy <stephenwang11@qq.com>
2019-12-18 16:44:29 +0800

    Use yarn to schedule mapreduce tasks

--- src/main/java/com/alibaba/hadoopdemo/hadoop/mapreduce/access/AccessYarnApp.java
+++ src/main/java/com/alibaba/hadoopdemo/hadoop/mapreduce/access/AccessYarnApp.java
@@ -1,13 +1,12 @@
 package com.alibaba.hadoopdemo.hadoop.mapreduce.access;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
 
 import java.io.IOException;
 
@@ -15,16 +14,19 @@ import java.io.IOException;
  * @Author: wjy
  * @Date: 2019/12/17 20:34
  */
-public class AccessLocalApp {
+public class AccessYarnApp {
 
-    //Driver 端的代码相对固定，不需要做太多的更改
+    /**
+     * Driver端代码相对固定不需要做太多改动
+     * @param args  args[0]表示输入路径  args[1]表示输出路径
+     */
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
         Job job = null;
         try {
             job = Job.getInstance(configuration);
             //设置要处理的类
-            job.setJarByClass(AccessLocalApp.class);
+            job.setJarByClass(AccessYarnApp.class);
 
             //设置mapper 和 reducer的类
             job.setMapperClass(AccessMapper.class);
@@ -41,30 +43,16 @@ public class AccessLocalApp {
             job.setOutputValueClass(Access.class);
 
             //设置输入输出的路径
-            FileInputFormat.setInputPaths(job, new Path("input/access/access.log"));
-            FileOutputFormat.setOutputPath(job, new Path("output/access/"));
+            FileInputFormat.setInputPaths(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        boolean result = false;
-
-        result = getResult(job);
+        boolean result = AccessLocalApp.getResult(job);
 
         System.out.println(result + "result");
 
-    }
-
-    public static boolean getResult(Job job) {
-        boolean result = false;
-        try {
-            if (job != null) {
-                result = job.waitForCompletion(true);
-            }
-        } catch (IOException | InterruptedException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return result;
     }
 }
